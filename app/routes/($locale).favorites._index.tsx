d5e766cfbc574f9f881a9acd8175c3eb842706e6
Fredy <fredzuna@hotmail.com>
2024-05-26 22:51:46 -0400

    add favorite page to display favorite list

---
+++ app/routes/($locale).favorites._index.tsx
@@ -0,0 +1,31 @@
+import {useLoaderData} from '@remix-run/react';
+import {defer, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import FavoriteItem from '~/components/favorites/FavoriteItem';
+
+// Creating new page to display favorite list
+export async function loader({context, request}: LoaderFunctionArgs) {
+  const {favoriteApi} = context; // We are connecting to the favorite API to check if the product is favorited.
+  const favorites = await favoriteApi.get('api/favorite');
+  return defer({favorites});
+}
+
+export default function Favorites() {
+  const {favorites} = useLoaderData<typeof loader>();
+
+  return (
+    <div className="collections">
+      <h1>Favorite products</h1>
+      <div className="products-grid">
+        {favorites.map((favorite: any, index: number) => {
+          return (
+            <FavoriteItem
+              key={favorite.id}
+              favorite={favorite}
+              loading={index < 8 ? 'eager' : undefined}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
